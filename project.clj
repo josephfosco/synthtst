f4ba9891140d19587b0650ebb13c36a6762f4195
Joseph Fsoco <joseph@josephfosco.com>
2018-07-30 09:06:49 -0500

    starting multichannel ctl bus in fm-morph

--- project.clj
+++ project.clj
@@ -16,8 +16,8 @@
 (defproject synthtst "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU GENERAL PUBLIC LICENSE version 3"
+            :url "http://www.gnu.org/licenses/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [overtone "0.10.3"]
                  ]
--- src/synthtst/fm_morph.clj
+++ src/synthtst/fm_morph.clj
@@ -419,7 +419,7 @@
 (def cntl-parms [
                 {:out-mod-lvl0 0 :out-mod-lvl1 0 :freq-ratio 1 :vol 1}
                 {:out-mod-lvl0 0 :out-mod-lvl1 0 :freq-ratio 7 :vol 0}
-                {:out-mod-lvl0 0 :out-mod-lvl1 0 :freq-ratio 1.42 :vol 0}
+                {:out-mod-lvl0 500 :out-mod-lvl1 0 :freq-ratio 1.42 :vol 0}
                 ])
 
 (def cntl-synths
@@ -590,3 +590,238 @@
 ;;    :out-bus-num 0
 ;;    :freq-bus-num (:id (tfreq-buses 0)))
 ;;   )
+
+;;-----------------------------------------------------------------
+;; TRY MULTI-CHANNEL CONTROL BUS
+;;-----------------------------------------------------------------
+
+
+(defonce fm-main-g (group "fm-main"))
+(defonce fm-early-g (group "fm early" :head fm-main-g))
+(defonce fm-later-g (group "fm later" :after fm-early-g))
+
+(defonce main-audio-bus (audio-bus 1 "fm-audio-bus"))
+
+(defsynth main-out-synth
+  []
+  (out [0 1]
+       (limiter (in main-audio-bus) 0.9 0.01)
+       )
+  )
+
+(def fm-main-out (main-out-synth [:tail fm-later-g]))
+
+(def num-operators 8)
+(def num-cntl-buses 10)
+
+(defonce fm-mod-buses (vec (for [i (range num-operators)]
+                              (audio-bus 1 (str "fm-mod-bus" i)))))
+(defonce feedback-buses (vec (for [i (range num-operators)]
+                                (audio-bus 1 (str "feedback-bus" i)))))
+
+(defsynth feedback-synth
+  [inbus 3 outbus 3]
+  (let [input (in-feedback:ar inbus)]
+    (out:ar outbus input)
+    ))
+
+;; feedback-synths move audio data from the fm-mod-busses to the
+;; feedback-busses. This is done so that all operators can access the output
+;; of all other operators (on the feedback-busses). Without this, an
+;; individual operator would only be able to use the output of operators
+;; defined after it was. This works because feedback-synth uses the
+;; in-feedback ugen.
+(def feedback-synths (vec (for [i (range num-operators)]
+                        (feedback-synth [:head fm-early-g]
+                                        :inbus (fm-mod-buses i)
+                                        :outbus (feedback-buses i))
+                        )))
+
+(defonce base-freq-bus (control-bus 1 "base-freq-bus"))
+(control-bus-set! base-freq-bus 110)
+
+;; Creates a vector of num-operators vectors with each internal vector
+;; having num-cntl-buses control-buses
+(def cntl-buses
+  (vec (for [opr (range num-operators)]
+         (control-bus num-cntl-buses (str opr "-"))
+         ))
+  )
+
+(defsynth mod-lvls-synth
+  [
+   out-bus 0
+   out-mod-lvl0 0
+   out-mod-lvl1 0
+   out-mod-lvl2 0
+   out-mod-lvl3 0
+   out-mod-lvl4 0
+   out-mod-lvl5 0
+   out-mod-lvl6 0
+   out-mod-lvl7 0
+   morph-time 1
+   ]
+  (let [
+        o-ml0 (lag3:kr out-mod-lvl0 morph-time)
+        o-ml1 (lag3:kr out-mod-lvl1 morph-time)
+        o-ml2 (lag3:kr out-mod-lvl2 morph-time)
+        o-ml3 (lag3:kr out-mod-lvl3 morph-time)
+        o-ml4 (lag3:kr out-mod-lvl4 morph-time)
+        o-ml5 (lag3:kr out-mod-lvl5 morph-time)
+        o-ml6 (lag3:kr out-mod-lvl6 morph-time)
+        o-ml7 (lag3:kr out-mod-lvl7 morph-time)
+        ]
+    (out:kr out-bus [o-ml0 o-ml1 o-ml2 o-ml3 o-ml4 o-ml5 o-ml6 o-ml7])
+    )
+  )
+
+(defsynth cntl-synth
+  [
+   out-bus-num 0
+   freq-ratio 1
+   volume 1
+   morph-time 1
+   ]
+  (let [
+        fr (lag3:kr freq-ratio morph-time)
+        vol (lag3:kr volume morph-time)
+        ]
+    (out:kr out-bus-num [fr vol])
+    )
+  )
+
+(def cntl-parms [
+                 {:out-mod-lvl0 0   :out-mod-lvl1 0
+                  :out-mod-lvl2 0
+                  :freq-ratio 1 :vol 1
+                  }
+                 {:out-mod-lvl0 500 :out-mod-lvl1 0
+                  :out-mod-lvl2 0
+                  :freq-ratio 2 :vol 0
+                  }
+                 {:out-mod-lvl0 0   :out-mod-lvl1 400
+                  :out-mod-lvl2 0
+                  :freq-ratio 3 :vol 0
+                  }
+                 {:out-mod-lvl0 0   :out-mod-lvl1 0
+                  :out-mod-lvl2 300
+                  :freq-ratio 4 :vol 0
+                  }
+                 {:out-mod-lvl0 0   :out-mod-lvl1 0
+                  :out-mod-lvl2 0
+                  :freq-ratio 5 :vol 0
+                  }
+                 {:out-mod-lvl0 0   :out-mod-lvl1 0
+                  :out-mod-lvl2 0
+                  :freq-ratio 6 :vol 0
+                  }
+                 {:out-mod-lvl0 0   :out-mod-lvl1 0
+                  :out-mod-lvl2 0
+                  :freq-ratio 7 :vol 0
+                  }
+                 {:out-mod-lvl0 0   :out-mod-lvl1 0
+                  :out-mod-lvl2 0
+                  :freq-ratio 8 :vol 0
+                  }
+                ])
+
+; These indexes must match the order the control-buses are listed in the
+;; "out:kr" statement of the cntl-synth
+(def out-mod-lvl0-ndx 0)
+(def out-mod-lvl1-ndx 1)
+(def out-mod-lvl2-ndx 2)
+(def freq-ratio-ndx 8)
+(def volume-ndx 9)
+
+(def mod-lvl-synths
+  (vec (for [i (range num-operators)]
+         (let [parms (cntl-parms i)]
+           (mod-lvls-synth [:head fm-early-g]
+                       (cntl-buses i)
+                       (or (:out-mod-lvl0 parms) 0)
+                       (or (:out-mod-lvl1 parms) 0)
+                       (or (:out-mod-lvl2 parms) 0)
+                       (or (:out-mod-lvl3 parms) 0)
+                       (or (:out-mod-lvl4 parms) 0)
+                       (or (:out-mod-lvl5 parms) 0)
+                       (or (:out-mod-lvl6 parms) 0)
+                       (or (:out-mod-lvl7 parms) 0)
+                       ))
+         ))
+  )
+
+(def cntl-synths
+  (vec (for [i (range num-operators)]
+         (let [parms (cntl-parms i)
+               cntl-bus-num (+ (:id (cntl-buses i)) freq-ratio-ndx)]
+           (cntl-synth [:head fm-early-g]
+                       cntl-bus-num
+                       (:freq-ratio parms)
+                       (:vol parms)
+                       ))
+         ))
+  )
+
+(defsynth fm-oper
+  [
+   in-mod-bus 3
+   out-mod-bus 2
+   cntl-bus 7
+   action NO-ACTION
+   gate 0
+   ]
+  (let [
+        [mod-lvl0
+         mod-lvl1
+         mod-lvl2
+         mod-lvl3
+         mod-lvl4
+         mod-lvl5
+         mod-lvl6
+         mod-lvl7
+         vol
+         freq-ratio
+         ] (in:kr cntl-bus num-cntl-buses)
+        envelope (env-gen (perc 5.0 5.0) gate 1 0 1 action)
+        out-osc (* (sin-osc :freq (+ (* (in:kr base-freq-bus) freq-ratio)
+                                     (in:ar in-mod-bus)))
+                   envelope
+                   )
+        ]
+    (out:ar out-mod-bus
+            [
+             (* out-osc mod-lvl0)
+             (* out-osc mod-lvl1)
+             (* out-osc mod-lvl2)
+             ])
+    (out:ar main-audio-bus (* out-osc vol))
+    ))
+
+(def fm-voice
+  (for [oper-id (range num-operators)]
+      (fm-oper [:tail fm-early-g]
+               :in-mod-bus (feedback-buses oper-id)
+               :out-mod-bus (fm-mod-buses 0)
+               :cntl-bus (cntl-buses oper-id)
+               )
+    ))
+
+(doseq [oper fm-voice]
+  (ctl oper :gate 1 :action FREE)
+  )
+
+(ctl (cntl-synths 0) :freq-ratio 1)
+(ctl (cntl-synths 0) :out-mod-lvl 0)
+(ctl (cntl-synths 0) :volume 1)
+(ctl (cntl-synths 1) :freq-ratio 1.42)
+(ctl (cntl-synths 1) :out-mod-lvl0 500)
+(ctl (cntl-synths 1) :volume 0)
+(ctl (cntl-synths 2) :out-mod-lvl0 0)
+(control-bus-get ((cntl-buses 0) 0))
+(control-bus-get ((cntl-buses 0) 1))
+(control-bus-get ((cntl-buses 0) 2))
+(control-bus-get ((cntl-buses 1) 0))
+(control-bus-get ((cntl-buses 1) 1))
+(control-bus-get ((cntl-buses 1) 2))
+(control-bus-set! base-freq-bus 110)
+(stop)
